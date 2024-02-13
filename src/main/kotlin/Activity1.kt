d0cd110896b324b2d9fc8f2202ae3addb73a740d
SalazrJohnRyn <j.ryn143@gmail.com>
2024-02-13 19:12:16 +0800

    Activity 1

--- src/main/kotlin/Activity1.kt
+++ src/main/kotlin/Activity1.kt
@@ -1,3 +1,19 @@
 package org.example
 
-class Activity1 {}
+fun main () {
+
+
+   for(i in 1..4) {
+      println("Enter your first name: ")
+      var fName = readLine()
+      println("Enter your middle initial:")
+      var mi = readLine()
+      println("Enter your last name: ")
+      var lName = readLine()
+      println("Enter your age:")
+      var age = readLine()?.toInt() ?: 0
+
+      println("Your name is $fName $mi $lName")
+      println("Your age is $age")
+   }
+}
