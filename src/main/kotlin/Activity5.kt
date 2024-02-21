ecfe3620d02aca1dd8ea5de9909dee23e5382307
John Ryn Salazar <j.ryn143@gmail.com>
2024-02-21 21:34:31 +0800

    Activity5 (Student library)

--- src/main/kotlin/Activity5.kt
+++ src/main/kotlin/Activity5.kt
@@ -3,19 +3,35 @@ package src.main.kotlin
 import kotlin.system.exitProcess
 
 class BookList() {
+    var book: String? = null
+    //
 
-   //
-   var book1 = "CLEAN CODE: A handbook of Agile Software Craftsmanship by Robert C. Martin"
-        val book2 = "INTRODUCTION TO ALGORITHMS by Thomas Cormen, Charles Leiserson, Ronald Rivest and Clifford Stein"
-        val book3 =
-            "STRUCTURE AND INTERPRETATION OF COMPUTER SCIENCE by Harold Abelson, Gerald Jay Sussman, Julie Sussman"
-        val book4 = "THE CLEAN CODER: A code of conduct for professional Programmers by Robert Martin"
-        val book5 = "CODE COMPLETE: A Practical Handbook of Software Construction by Steve McConnell"
-        val book6 = "DESIGN PATTERNS: Elements of Reusable Object-Oriented Software by Erich Gamma and team"
-        val book7 = "THE PRAGMATIC PROGRAMMER by Andrew Hunt, David Thomas"
-        val book8 = "REFACTORING: Improving the Design of Existing Code by Martin Fowler"
-   //
+    fun books(){
+        println("----------------------------------------------------------\n"+
+                "PLEASE CHOOSE AN OPTION OF WHAT BOOK DO YOU WANT TO BORROW\n" +
+                "----------------------------------------------------------\n"+
+                "[1] CLEAN CODE: A handbook of Agile Software Craftsmanship by Robert C. Martin\n" +
+                "[2] INTRODUCTION TO ALGORITHMS by Thomas Cormen, Charles Leiserson, Ronald Rivest and Clifford Stein\n" +
+                "[3] STRUCTURE AND INTERPRETATION OF COMPUTER SCIENCE by Harold Abelson, Gerald Jay Sussman, Julie Sussman\n" +
+                "[4] THE CLEAN CODER: A code of conduct for professional Programmers by Robert Martin\n" +
+                "[5] CODE COMPLETE: A Practical Handbook of Software Construction by Steve McConnell\n" +
+                "[6] DESIGN PATTERNS: Elements of Reusable Object-Oriented Software by Erich Gamma and team\n" +
+                "[7] THE PRAGMATIC PROGRAMMER by Andrew Hunt, David Thomas\n" +
+                "[8] REFACTORING: Improving the Design of Existing Code by Martin Fowler"
+        )
+        val ans: Int? = readLine()?.toInt()
+        when (ans) {
+            1 -> book = "CLEAN CODE: A handbook of Agile Software Craftsmanship by Robert C. Martin"
+            2 -> book = "INTRODUCTION TO ALGORITHMS by Thomas Cormen, Charles Leiserson, Ronald Rivest and Clifford Stein"
+            3 -> book = "STRUCTURE AND INTERPRETATION OF COMPUTER SCIENCE by Harold Abelson, Gerald Jay Sussman, Julie Sussman"
+            4 -> book = "THE CLEAN CODER: A code of conduct for professional Programmers by Robert Martin"
+            5 -> book = "CODE COMPLETE: A Practical Handbook of Software Construction by Steve McConnell"
+            6 -> book = "DESIGN PATTERNS: Elements of Reusable Object-Oriented Software by Erich Gamma and team"
+            7 -> book = " THE PRAGMATIC PROGRAMMER by Andrew Hunt, David Thomas"
+            8 -> book = " REFACTORING: Improving the Design of Existing Code by Martin Fowler"
 
+        }
+    }
 }
 
 fun main() {
@@ -30,68 +46,19 @@ fun main() {
     val course = readLine().toString().uppercase()
     println("------------------------")
     do {
-        println(
-            "Please Choose a book you want to borrow:\n" +
-                    "[1] CLEAN CODE: A handbook of Agile Software Craftsmanship by Robert C. Martin\n" +
-                    "[2] INTRODUCTION TO ALGORITHMS by Thomas Cormen, Charles Leiserson, Ronald Rivest and Clifford Stein\n" +
-                    "[3] STRUCTURE AND INTERPRETATION OF COMPUTER SCIENCE by Harold Abelson, Gerald Jay Sussman, Julie Sussman\n" +
-                    "[4] THE CLEAN CODER: A code of conduct for professional Programmers by Robert Martin\n" +
-                    "[5] CODE COMPLETE: A Practical Handbook of Software Construction by Steve McConnell\n" +
-                    "[6] DESIGN PATTERNS: Elements of Reusable Object-Oriented Software by Erich Gamma and team\n" +
-                    "[7] THE PRAGMATIC PROGRAMMER by Andrew Hunt, David Thomas\n" +
-                    "[8] REFACTORING: Improving the Design of Existing Code by Martin Fowler"
-        )
-        val ans: Int? = readLine()?.toInt()
-        when (ans) {
-            1 -> {
-                println("Do you want to borrow ${a1.book1}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book1}]")
-
-            }
-
-            2 -> {
-                println("Do you want to borrow ${a1.book2}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book2}]")
-            }
-
-            3 -> {
-                println("Do you want to borrow ${a1.book3}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book3}]")
-            }
-
-            4 -> {
-                println("Do you want to borrow ${a1.book4}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book4}]")
-            }
-
-            5 -> {
-                println("Do you want to borrow ${a1.book5}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book5}]")
-            }
-
-            6 -> {
-                println("Do you want to borrow ${a1.book6}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book6}]")
-            }
-
-            7 -> {
-                println("Do you want to borrow ${a1.book7}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book7}]")
-            }
-
-            8 -> {
-                println("Do you want to borrow ${a1.book8}?: [Y/N]")
-                val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
-                if (ans1 == 'y') println("$name $course borrowed [${a1.book8}]")
-            }
-        }
+        do {
+            a1.books()
+            println("----------------------------------")
+            println("CONFIRMATION:")
+            println("----------------------------------")
+            println(
+                "Are you sure you want to borrow [${a1.book}]?\n" +
+                        "Type[Y] for yes and [N] for no."
+            )
+            val response: Char? = readLine()?.singleOrNull()?.lowercaseChar()
+            if (response == 'y') continue
+        }while(response == 'n')
+        println("$name a $course borrowed a book [${a1.book}]")
         println("Do you want to borrow more book?: [Y/N]")
         val ans1: Char? = readLine()?.singleOrNull()?.lowercaseChar()
         if (ans1 == 'y') continue
